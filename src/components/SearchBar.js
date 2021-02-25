06d8b66cc1f7c8acd8b196241243341f89c8fdb2
Nghia <hello.nghia22@gmail.com>
2021-02-24 21:56:56 -0500

    feat: Create SearchBar comp

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+    return (
+        <form>
+            <h1>Employee Directory</h1>
+            <input text="text"></input>
+        </form>
+    )
+}
+
+export default SearchBar;
