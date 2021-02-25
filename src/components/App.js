328353c4a84d017da2befc1d6206ed9446c789c9
Nghia <hello.nghia22@gmail.com>
2021-02-24 22:00:22 -0500

    feat: Add header and main tag to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import SearchBar from './SearchBar'
+import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <SearchBar/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<header>
+					<SearchBar />
+				</header>
+                <main>
+                    
+                </main>
+			</div>
+		);
+	}
 }
 
 export default App;
