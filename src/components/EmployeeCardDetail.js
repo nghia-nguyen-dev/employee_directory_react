c48751c3b4eed03631ce22f2d17057e3cc218ff5
Nghia <hello.nghia22@gmail.com>
2021-02-26 22:02:18 -0500

    feat: Add styling to detail card

--- src/components/EmployeeCardDetail.js
+++ src/components/EmployeeCardDetail.js
@@ -2,17 +2,21 @@ import React from "react";
 import "../css/EmployeeCardDetail.css";
 
 const EmployeeCardDetail = ({
-    // Destructuring the selectedEmployee obj in param
+	// Destructuring the selectedEmployee obj in param
 	selectedEmployee: {
 		employee: { picture, name, location, email, phone },
 		index,
 	},
 }) => {
-    // Function body
+	// Function body
 	return (
 		<div className="employee-card-detail" data-key={index}>
-			<img src={picture.medium} alt="employee picture" />
-			<h2>{`${name.first} ${name.last}`}</h2>
+			<img
+				className="employee-card-detail__img"
+				src={picture.large}
+				alt="employee picture"
+			/>
+			<h2 className="employee-card-detail__name">{`${name.first} ${name.last}`}</h2>
 			<div className="employee-card-detail__contact">
 				<p>{email}</p>
 				<p>{phone}</p>
